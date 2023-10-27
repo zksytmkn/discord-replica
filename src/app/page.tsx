b747ac97d9bb44a587edb698524d367673169c31
nakamoto <zksytmkn4869@gmail.com>
2023-10-28 00:32:39 +0900

    feat:Implement login using Supabase's authentication features#29

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Chat from "@/components/chat/chat";
 import Login from "@/components/login/login";
 import Sidebar from "@/components/sidebar/sidebar";
 
-export default function Home() {
-  const user = null;
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
   return (
     <>
       <div className="flex overflow-y-hidden">
-        {user ? (
+        {session ? (
           <>
             <Sidebar />
             <Chat />
