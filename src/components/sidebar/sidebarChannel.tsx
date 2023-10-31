5ca3575be4af3846999a29957017e21d6a306499
nakamoto <zksytmkn4869@gmail.com>
2023-11-01 01:41:09 +0900

    feat:Channel data-related configuration#16

--- src/components/sidebar/sidebarChannel.tsx
+++ src/components/sidebar/sidebarChannel.tsx
@@ -1,9 +1,18 @@
-export default function SidebarChannel() {
+import { Channel } from "@/types/channel";
+
+type Props = {
+  id: number;
+  channel: Channel;
+};
+
+export default function SidebarChannel(props: Props) {
+  const { id, channel } = props;
+
   return (
     <div className="pl-5 mt-1">
       <h4 className="text-[#7b7c85] flex items-center p-1 cursor-pointer hover:text-white hover:bg-[#33363d] hover:rounded-md">
         <span className="text-xl pr-2">#</span>
-        Udemy
+        {channel.name}
       </h4>
     </div>
   );
